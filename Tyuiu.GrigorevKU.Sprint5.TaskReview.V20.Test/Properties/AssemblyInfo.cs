fd86e1368c98752ce3a3a1bd596e2b768ab95fbb
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-12-01 12:11:03 +0500

    Add project files.

---
+++ Tyuiu.GrigorevKU.Sprint5.TaskReview.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint5.TaskReview.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint5.TaskReview.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da580279-f16e-4dfe-b89f-84daabd36f82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GrigorevKU.Sprint5.TaskReview.V20/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.GrigorevKU.Sprint5.TaskReview.V20.Lib;
+
+namespace Tyuiu.GrigorevKU.Sprint5.TaskReview.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Григорьев К.Ю. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: TaskReview                                                        *");
+            Console.WriteLine("* Задание #TaskReview                                                     *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Григорьев Константин Юрьевич | ИИПб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая заменяет все удвоенные буквы 'сс' на 'с'.   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V20.txt";
+            string pathSaverFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V20.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле: ");
+            pathSaverFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaverFile);
+            Console.ReadKey();
+        }
+    }
+}
